5ee4b3e6159f53ddbfd15c41b599d5505d0c1c69
3p02 <bobbyfngaming@gmail.com>
2025-12-15 14:19:05 +0100

    Aktualizacja

---
+++ Egzamin/ 36 inf_03 2024_06_01_php/motor.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Motocykle</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+<img src="img/motor.png" alt="motocykl" id="motor">
+<header id="baner">
+    <h1>Motocykle - moja pasja</h1>
+</header>
+<section id="lewy">
+    <h2>Gdzie pojechać?</h2>
+</section>
+<section id="prawy1">
+    <h2>Co kupić?</h2>
+    <ol>
+        <li>Honda CBR125R</li>
+        <li>Yamaha YBR125</li>
+        <li>Honda VFR800i</li>
+        <li>Honda CBR1100XX</li>
+        <li>BMW R1200GS LC</li>
+    </ol>
+</section>
+<section id="prawy2">
+    <h2>Statystyki</h2>
+    <p>Wpisanych wycieczek:</p>
+    <p>Użytkowników forum: 200</p>
+    <p>Przesłanych zdjęć: 1300</p>
+</section>
+<footer id="stopka">
+    <p>Stronę wykonał: Seweryn Poczynek 3P</p>
+</footer>
+</body>
+</html>
+<?php
+
+?>
+
+<?php
+
+?>
